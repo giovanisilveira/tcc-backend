4aa7b34167d37f0af9d4afb9030c793ffe8b5255
Giovani Silveira <giovanisis@gmail.com>
2019-04-17 08:48:00 -0300

    Update: - Desabilitar cache

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -11,6 +11,9 @@ chdir(__DIR__ . '/..');
 define("CONF_FILE", "conf/config.ini");
 define("HASH_SENHA", "TccPUCGiovani");
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 require 'vendor/autoload.php';
 
